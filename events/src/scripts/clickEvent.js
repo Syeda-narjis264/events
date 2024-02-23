fd1aad137464e7e24c77daf662c38cf39dd52c8a
Syeda-narjis264 <155845709+Syeda-narjis264@users.noreply.github.com>
2024-02-23 05:57:44 -0800

    Add files via upload

---
+++ events/src/scripts/clickEvent.js
@@ -0,0 +1,10 @@
+const noteButton = document.getElementById("button")
+console.log(noteButton)
+
+const handleButtonClick = ()=>{
+    console.log("buttonclicker")
+}
+
+
+
+noteButton.addEventListener("click", handleButtonClick)
