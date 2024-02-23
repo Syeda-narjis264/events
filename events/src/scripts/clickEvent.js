b1a47d0f8632ca1c20fd8084f6f91671796dd1f9
Syeda-narjis264 <syedanarjis031@gmail.com>
2024-02-23 06:48:13 -0800

    change

--- events/src/scripts/clickEvent.js
+++ events/src/scripts/clickEvent.js
@@ -1,10 +1,18 @@
-const noteButton = document.getElementById("button")
-console.log(noteButton)
+// const noteButton = document.getElementById("button")
+// console.log(noteButton)
 
-const handleButtonClick = ()=>{
-    console.log("buttonclicker")
-}
+// const handleButtonClick = ()=>{
+//     console.log("buttonclicker")
+// }
 
 
 
-noteButton.addEventListener("click", handleButtonClick)
+// noteButton.addEventListener("click", handleButtonClick)
+
+function printFormData() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+  }
